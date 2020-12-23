a484dfd6989d97a80724d6dd8de2dce0d88fac8f
Jake Vis <visser.jacob@outlook.com>
2020-12-23 12:09:36 -0800

    suppot for hostsfile

--- start-rsyslog.sh
+++ start-rsyslog.sh
@@ -2,4 +2,5 @@
 
 cp /config/config.php /opt/observium/config.php
 rsyslogd
+cat /config/hosts >> /etc/hosts
 tail -F /var/log/syslog
--- start.sh
+++ start.sh
@@ -3,4 +3,5 @@
 cp /config/config.php /opt/observium/config.php
 cron -f -l 8 -L /dev/stdout &
 rsyslogd &
+cat /config/hosts >> /etc/hosts
 apachectl -D FOREGROUND
